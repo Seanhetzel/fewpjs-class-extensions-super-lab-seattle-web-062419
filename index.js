086420d28a4b09d920350d432231fec5855a64e9
Sean Hetzel <seanhetzel1@gmail.com>
2019-08-16 16:59:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ class Tree {
   }
 }
 
-class Deciduous {
+class Deciduous extends Tree {
   constructor(species, name) {
     super(species)
     this.name = name
   }
 
   static definition(){
-    return (super.definition() + "Deciduous trees shed their leaves annually.")
+    return super.definition() + "Deciduous trees shed their leaves annually."
   }
 }
